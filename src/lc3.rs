06a1b86e15f122e32dff32030a01211137e75596
AN3223 <35916076+AN3223@users.noreply.github.com>
2019-02-08 15:01:34 -0600

    Added load indirect

--- src/lc3.rs
+++ src/lc3.rs
@@ -1,4 +1,5 @@
-use crate::{U16_MAX, RCOND, FL, sign_extend, check_key, MR};
+use crate::{U16_MAX, RCOND, FL, sign_extend, check_key, MR, Register};
+use Register::*;
 
 // Represents the whole LC-3
 pub struct LC3 {
@@ -40,28 +41,36 @@ impl LC3 {
     }
 
     pub fn add(&mut self, instruction: u16) {
-        let destination = (instruction >> 9) & 0x7; // Destination register
-        let sr1 = (instruction >> 6) & 0x7; // First operand
+        let destination_register = instruction >> 9 & 0x7;
+        let sr1 = instruction >> 6 & 0x7; // First operand
 
-        let immflag = (instruction >> 5) & 0x1; // Determines what the second operand is
+        let immflag = instruction >> 5 & 0x1; // Determines what the second operand is
         let operand = {
             if immflag == 1 {
                 sign_extend(instruction & 0x1F, 5)
-                // imm5 is 5 bits, so we extract those bits and sign_extend them
             } else {
                 self.registers[(instruction & 0x7) as usize] // SR2
             }
         };
 
         // Here's where the actual addition happens
-        self.registers[destination as usize] = {
+        self.registers[destination_register as usize] = {
             self.registers[sr1 as usize] + operand
         };
 
-        self.update_rcond(destination as usize);
+        self.update_rcond(destination_register as usize);
     }
 
+    // Load indirect
     pub fn ldi(&mut self, instruction: u16) {
-        
+        let pcoffset = sign_extend(instruction & 0x1ff, 9);
+        let destination_register = instruction >> 9 & 0x7;
+
+        let location = self.get_memory(
+            (self.registers[RPC as usize] + pcoffset) as usize
+        );
+        self.registers[pcoffset as usize] = self.get_memory(location as usize);
+
+        self.update_rcond(destination_register as usize);
     }
 }
